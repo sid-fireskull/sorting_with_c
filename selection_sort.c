99e3e30e3f90324b6f350f25e20a5e15f3093811
Sid <siddharthasadhukhan2009@gmail.com>
2019-06-10 14:45:32 +0530

    Selection Sort

---
+++ selection_sort.c
@@ -0,0 +1,42 @@
+// Selection Sort using array
+
+#include<stdio.h>
+
+int main()
+{
+	int array[5];
+	int i, j;
+	
+	printf("Enter the series of number of the array: \n");
+	
+	for(i=0;i<5;i++)
+	{
+		scanf("%d", &array[i]);
+	}
+	
+	
+	// Sorting smallest element to the left side of the array
+	for(i=0; i<5;i++)
+	{
+		for(j=i+1;j<5;j++)
+		{
+			if(array[i] > array[j])
+			{
+				array[j] = array[i]+ array[j];
+				array[i] = array[j]-array[i];
+				array[j] = array[j]-array[i];
+			}
+		}
+	}
+	
+	printf("Sorted Array:");
+	
+	for(i=0;i<5;i++)
+	{
+		printf("%3d", array[i]);
+	}
+
+	getch();
+}
+
+
